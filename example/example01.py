d8b492ccfb550548a06ced25251d71769f6bca81
cks1031 <cks1031@naver.com>
2024-07-11 11:11:34 +0900

    연습문제 풀이 - 만나이 계산, 잔돈 계산

--- example/example01.py
+++ example/example01.py
@@ -169,7 +169,7 @@ print(name,weight,age)
 # {dan} x 8 = {dan * 8}
 # {dan} x 9 = {dan * 9}''')
 #
-# x = int(input('단수'))
-# for y in range(1,10):
-#     print(x, "*", y ,"=" , x * y)
+x = int(input('단수'))
+for y in range(1,20):
+print(x, "*", y ,"=" , x * y)
 
