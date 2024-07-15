dbe31a2fba75bb9a8d750e7303af50150528d086
cks1031 <cks1031@naver.com>
2024-07-15 09:54:55 +0900

    성적처리 프로그램 v3 - for 반복문을 이용한 학생 3명 성적 처리

--- 10SungJukV3.py
+++ 10SungJukV3.py
@@ -20,29 +20,32 @@ grds = []
 
 # 성적데이터 입력받음
 for i in range(3):
-        names.append(input('학생 이름은?'))
-        kors.append(int(input('학생 국어 점수는?')))
-        engs.append(int(input('학생 영어 점수는?')))
-        mats.append(int(input('학생 수학 점수는?')))
-
+        names.append(input(f'{i+1}번학생 이름은?'))
+        kors.append(int(input(f'{i+1}번학생 국어 점수는?')))
+        engs.append(int(input(f'{i+1}번학생 영어 점수는?')))
+        mats.append(int(input(f'{i+1}번학생 수학 점수는?')))
 # 성적 처리
 for i in range(3):
         tot = kors[i] + engs[i] + mats[i]
         tots.append(tot)
-        avg = tot / 3
+        avg = tots[i] /3
         avgs.append(avg)
         grd = '수' if (avg >= 90) else \
                 '우' if (avg >= 80) else\
                 '미' if (avg >= 70) else\
                 '양' if (avg >= 70) else '가'
         grds.append(grd)
-
 # 결과 출력
+result = ''
 for i in range(3):
-        print(f'''
-        이름: {names[i]}, 국어: {kors[i]}, 영어: {engs[i]}, 수학: {mats[i]}
-        총점: {tots[i]} 평균: {avgs[i]:.1f} 학점 :{grds[i]}
-        ''')
+        result += f'''이름: {names[0]}, 국어: {kors[0]}, 영어: {engs[0]}, 수학: {mats[0]}
+        총점: {tots[0]} 평균: {avgs[0]:.1f} 학점 :{grds[0]}\n'''
+print(result)
+
+for i in range(3):
+   print(f'''
+   이름: {names[i]}, 국어: {kors[i]}, 영어: {engs[i]}, 수학: {mats[i]}
+   총점: {tots[i]} 평균: {avgs[i]:.1f} 학점 :{grds[i]}''')
 
 # 369게임 만들기 3,6,9 나올 때 마다 짝!
 x = '짝!'
