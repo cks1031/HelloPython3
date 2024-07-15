56c7029a4227cddd519211bee0e50bf384ec2ad7
cks1031 <cks1031@naver.com>
2024-07-15 15:10:05 +0900

    연습문제 추가

--- 09loop.py
+++ 09loop.py
@@ -59,4 +59,118 @@ for i in range(2, 101, 2):
     sum += i
 print(sum)
 
-print(sum)
+# 메일 발송
+
+count = int(input('메일 발송 횟수를 입력하세요: '))
+for _ in range(count):
+    print('메일 발송!')
+
+# 3의 배수 출력하기
+result = ''
+for i in range(1, 11):
+    result += f'num = {i}\n'
+    if i % 3 == 0:
+        result += f'3의 배수!!\n'
+
+print(result)
+
+# 5구구단 5단 출력하기
+# dan = int(input('단 수 : '))
+dan = 5
+result =''
+for i in range(1, 10):
+    result += f'{dan} x {i} = {dan * i}\n'
+print(result)
+
+# 줄 바꿈 없이 출력하기 (end='')
+result = ''
+for i in range (1, 11):
+    # print(i, end=' ')
+    result += f'{i} '
+print(result)
+
+# 1~100까지 정수 중에서 3과 7의 공배수와 최소공배수를 출력
+
+result = ''
+
+for i in range (1, 101):
+    if (i % 3 == 0) and (i % 7 == 0) :
+        result += f'{i} '
+
+print(result, f'[{3*7}]')
+
+# range함수 사용 예시
+print(list(range(-10, 0, 1)))
+print(list(range(10, 0, -1)))
+
+# 문자열을 for문에 사용하기
+str1 = 'Hello, World!!'
+
+for c in str1:
+    print(c, end=' ')
+
+# 369 게임
+# 1 ~ 10 : 3, 6, 9
+# 11 ~ 20:
+# ...
+# 31 ~ 39 : 31, ..33,..36,..39
+
+for i in range (1, 100+1):
+    if i < 10:
+        if i % 3 == 0:
+            print(f'{i} 짝!')
+        else:
+            print(i)
+    else:
+        clap = ''
+        fnum = i // 10
+        lnum = i % 10
+
+        if fnum % 3 == 0: clap += '짝! '
+        if lnum % 3 == 0 and lnum != 0: clap += '짝! '
+
+        print(f'{i} {clap}')
+
+# 열차 교차 시간 알아내기
+# 오전 9시 - 오후 6시간 (9시간 - 540분)
+# a열차: 10분 간격 , b열차: 25분 간격, c열차: 30분 간격
+trainA = 10
+trainB = 25
+trainC = 30
+
+for min in range (1, 540+1):
+    if min % trainA == 0 and min % trainB == 0: # 50분 간격
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : A - B 교차!')
+    elif min % trainB == 0 and min % trainC == 0:
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : B - A 교차!')
+    elif min % trainA == 0 and min % trainC == 0: # 30분 간격
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : C - A 교차!')
+
+# 로그인 기능 만들기
+isLogin = False
+
+for i in range(5):
+    # if isLogin == False:
+    if not isLogin:
+        passwd = input('관리자 암호를 입력하세요. ')
+        if passwd == 'hanbitac':
+            isLogin = True
+            print('로그인 되었습니다.')
+        else:
+            print('암호를 다시 입력하세요!')
+
+if not isLogin: print('로그인 실패! 횟수 초과!')
+
+
+
+
+
+
+
+
