d879597cb110d0e07b5c69de16ceff93a9d65b63
cks1031 <cks1031@naver.com>
2024-07-17 11:09:55 +0900

    연습문제 추가

--- 16dict.py
+++ 16dict.py
@@ -48,3 +48,148 @@ for idx, k in enumerate(member):
 # key와 value를 한 번에 출력 : 변수명.items
 for k, v in member.items():
     print(k, v)
+
+# 중간고사 성적 관리
+# 시나리오 #1
+mids = {'C/C++':'A', 'Java':'B+', '모바일':'C', '보안':'A+', '해킹':'F', '시스템':'C+'}
+print(mids)
+
+# 시나리오 #2
+print(f"Java : {mids['Java']}")
+print(f"시스템 : {mids['시스템']}")
+
+# 시나리오 #3
+mids['파이썬'] = 'A'
+mids['OS'] = 'A+'
+print(mids)
+
+# 시나리오 #4
+mids['Java'] = 'F'
+mids['시스템'] = 'A'
+print(mids)
+
+# 시나리오 #5
+for k in mids.keys():
+    print(f'{k} \t: {mids[k]}')
+
+for k, v in mids.items():
+    print(f'{k} \t: {v}')
+
+# 수학시험 프로그램 1
+quizs = ['3+2','5나누기2의몫','10-2','10의2제곱x2','1-(10나누기4의 나머지)','2의4제곱','4/2']
+answers = [5, 2, 8, 200, -1, 16, 2]
+score = [3,5,3,5,5,3,3]
+myAnswers = []
+
+trueCount = 0
+falseCount = 0
+totalScore = 0
+
+# 문제풀고 내 정답 입력
+for i in range(len(quizs)):
+    print(f'문제: {quizs[i]} ?')
+    myAnswers.append(int(input('정답을 입력하세요 : ')))
+
+# 문제 풀이 및 채점
+for i in range(len(answers)):
+    if answers[i] == myAnswers[i]:
+        trueCount += 1
+        totalScore += score[i]
+    else:
+        falseCount += 1
+
+# 결과 출력
+print(f'''
+{'-'*20}
+정답 개수: {trueCount}
+오답 개수: {falseCount}
+Total Score: {totalScore}
+{'-'*20}
+''')
+
+# 수학시험 프로그램 2 => 사용시 () 튜플로 저장
+quizs = (('3+2', 5, 3), ('5나누기2의몫', 2, 5),
+         ('10-2', 8, 3), ('10의2제곱x2', 200, 5),
+         ('1-(10나누기4의 나머지)', -1, 5),
+         ('2의4제곱', 16, 3), ('4/2', 2, 3))
+myAnswers = []
+
+trueCount = 0
+falseCount = 0
+totalScore = 0
+
+# 문제풀고 내 정답 입력
+for q in quizs:
+    print(f'문제: {q[0]} ?')
+    myAnswers.append(int(input('정답을 입력하세요 : ')))
+
+# 문제 풀이 및 채점
+# enumerate : 반복가능한 객체를 순회하면서
+# 각 요소의 위치값(인덱스)와 값을 함께 반환하는 함수
+for idx, q in enumerate(quizs):
+    if q[1] == myAnswers[idx]:
+        trueCount += 1
+        totalScore += q[2]
+    else:
+        falseCount += 1
+
+# 결과 출력
+print(f'''
+{'-'*20}
+정답 개수: {trueCount}
+오답 개수: {falseCount}
+Total Score: {totalScore}
+{'-'*20}
+''')
+
+# 수학시험 프로그램 my code
+quizs = ['3+2','5*2의몫','10-2','(10**2)*2','1-(10%4)','2**4','4/2']
+answer = ['5', '2', '8', '200', '-1', '16', '2']
+score = [3,5,3,5,5,3,3]
+trueCount = 0
+falseCount = 0
+totalScore = 0
+result = ''
+for i in range(7):
+    result = input(f'문제: {quizs[i]}')
+    if result == answer[i]:
+        trueCount += 1
+        totalScore += score[i]
+    elif result != answer[i]:
+        falseCount += 1
+    print(f'''{quizs[i]} 
+정답을 입력하세요.: {answer[i]}''')
+print(f'''---------------------------------------
+정답 개수: {trueCount}
+오답 개수: {falseCount}
+Total Score: {totalScore}
+---------------------------------------
+''')
+
+# 로또 당첨 게임
+from random import sample
+lotto = []
+matchs = []
+# 숫자 입력받기
+print('1부터 45사이 정수 6개를 입력하세요.')
+for i in range(6):
+    lotto.append(int(input(f'Number {i+1} :')))
+
+# 로토 매직 넘버 생성
+masic = sample(range(1,45+1),6)
+
+# 당첨 여부 확인
+for l in lotto:     #[1,2,3,4,5,6]
+    if l in masic:
+        matchs.append(l)
+# for i in range(6):
+#     if lotto[i] in masic:
+#         matchs += 1
+
+#결과 출력
+print(f'''
+이번주 로또 번호 {masic}
+내가 입력한 번호 {lotto}
+일치하는 숫자 : {matchs}
+''')
+
